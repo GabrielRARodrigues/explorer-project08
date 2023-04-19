e00e464e19c5a05419de6c365c6fe14163e51d2a
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-04-19 10:03:02 -0300

    Continuando o desenvolvimento da lógica do contador

--- script.js
+++ script.js
@@ -11,12 +11,29 @@ const secondsDisplay = document.querySelector('.seconds')
 function countDown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
 
-    if (seconds <= 0) {
-      seconds = 60
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+
+      buttonPause.classList.add('hide')
+
+      buttonSet.classList.remove('hide')
+
+      buttonStop.classList.add('hide')
+      return
     }
 
-    secondsDisplay.textContent = seconds - 1
+    if (seconds <= 0) {
+      seconds = 2
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
     countDown()
   }, 1000)
 }
@@ -61,5 +78,5 @@ buttonSoundOn.addEventListener('click', function () {
 
 buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
